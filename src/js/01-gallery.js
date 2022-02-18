93cc9694d619f5908a6dff106cb3d65936adcbc3
NataliiaGalanjuk <Tasha.Galanjuk@gmail.com>
2022-02-18 18:40:16 +0200

    Homework 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarckup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarckup);
+
+function createGalleryCardsMarkup(items) {
+    return items
+        .map(({ preview, original, description }) => {
+        return `
+    <li>
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>
+    `;
+    })
+        .join('');
+}
+
+const showGallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const player = addEventListener(".vimeo-player")
+player.on('eventName', function(data) {
+    // data is an object containing properties specific to that event
+});
+
+// "videoplayer-current-time"
+
+player.setCurrentTime(30.456).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
